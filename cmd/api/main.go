47ae81b461ae73d5db1c8961f02b4a7a8d58175e
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2025-01-07 14:35:03 +0000

    :sparkles: feat: Carrega variáveis de ambiente do arquivo .env e adiciona dependência godotenv

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
