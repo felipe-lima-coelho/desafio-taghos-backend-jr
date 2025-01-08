cacdbce5bf0f64784ebd836147e2662f965be07a
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2025-01-08 08:33:05 +0000

    :sparkles: feat: Interface para o repositório dos livros

---
+++ internal/repository/book_repository.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"github.com/felipe-lima-coelho/desafio-taghos-backend-jr/internal/domain"
+
+	"gorm.io/gorm"
+)
+
+type BookRepository interface {
+	Create(book *domain.Book) error
+	FindByID(id string) (*domain.Book, error)
+	FindByTitle(title string) (*domain.Book, error)
+	FindAll() ([]*domain.Book, error)
+	Update(book *domain.Book) error
+	Delete(id string) error
+}
+
+type gormBookRepository struct {
+	db *gorm.DB
+}
+
+func NewBookRepository(db *gorm.DB) BookRepository {
+	return &gormBookRepository{db}
+}
