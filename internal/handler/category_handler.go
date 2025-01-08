f7baa311e994e3e8baf92ec90302286ead3cd042
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2025-01-08 18:08:55 +0000

    :sparkles: feat: Adiciona método para criar categoria com validação de entrada

--- internal/handler/category_handler.go
+++ internal/handler/category_handler.go
@@ -1,7 +1,12 @@
 package handler
 
 import (
+	"errors"
+	"net/http"
+
+	"github.com/felipe-lima-coelho/desafio-taghos-backend-jr/internal/domain"
 	"github.com/felipe-lima-coelho/desafio-taghos-backend-jr/internal/service"
+	"github.com/gin-gonic/gin"
 )
 
 type CategoryHandler struct {
@@ -10,4 +15,44 @@ type CategoryHandler struct {
 
 func NewCategoryHandler(categoryService service.CategoryService) *CategoryHandler {
 	return &CategoryHandler{categoryService}
+}
+
+func (h *CategoryHandler) CreateCategory(c *gin.Context) {
+	var category domain.Category
+	if err := c.ShouldBindJSON(&category); err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"error": gin.H{
+					"code":    "INVALID_REQUEST_BODY",
+					"message": "invalid request body",
+					"details": errors.New("Error while binding JSON: " + err.Error()),
+				},
+			},
+		)
+		return
+	}
+
+	if err := h.categoryService.CreateCategory(&category); err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"error": gin.H{
+					"code":    "CREATE_CATEGORY_ERROR",
+					"message": "error while creating category",
+					"details": errors.New("Error while creating category: " + err.Error()),
+				},
+			},
+		)
+		return
+	}
+
+	c.JSON(
+		http.StatusCreated,
+		gin.H{
+			"data": gin.H{
+				"category": category,
+			},
+		},
+	)
 }
