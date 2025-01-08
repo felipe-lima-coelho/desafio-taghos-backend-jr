fc350cbd187ccaeb28279955ddaa557ea27b30f8
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2025-01-08 18:31:01 +0000

    :sparkles: feat: Handler de autor com estrutura de solicitação e resposta

---
+++ internal/handler/author_handler.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"github.com/felipe-lima-coelho/desafio-taghos-backend-jr/internal/service"
+)
+
+type AuthorHandler struct {
+	authorService service.AuthorService
+}
+
+type authorRequest struct {
+	Name string `binding:"required"`
+}
+
+type authorResponse struct {
+	ID   string
+	Name string
+}
+
+func NewAuthorHandler(authorService service.AuthorService) *AuthorHandler {
+	return &AuthorHandler{authorService}
+}
